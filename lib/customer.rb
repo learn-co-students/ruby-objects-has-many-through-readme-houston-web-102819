62eab07f18c1c8b713ebdcba59b217a6a4bdfe56
Joseph Burks <joe_burks_3@yahoo.com>
2019-11-03 17:17:43 -0600

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,35 @@
+require ./lib/waiter.rb
+require ./lib/meal.rb
+
 class Customer
+    attr_accessor :name, :age
+
+    @@all = []
+
+    def initialize(name, age)
+        @name = name
+        @age = age
+        @@all.push(self)
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(waiter, total, tip=0)
+        Meal.new(waiter, self, total, tip)
+    end
+
+    def meals
+        Meal.all.select do |meal|
+            meal.custome == self
+        end
+    end
+
+    def waiters
+        meals.map do |meal|
+            meal.waiter
+        end
+    end
   
 end
